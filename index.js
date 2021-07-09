065d97ffba0f0196bac03cb026b008fdfa09f57b
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-07-09 08:05:06 -0700

    added code for server to listen on port 5000

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+const server = require("./api/server.js");
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, ()=>{
+    console.log(`\n***Server listening on port ${PORT}***\n`);
+})
