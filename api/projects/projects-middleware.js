d758b87e0218467908e3283e6fcc41c1d163f891
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-07-09 09:51:24 -0700

    first 3 projects api ends points are coded and working

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,33 @@
 // add middlewares here related to projects
+const pDb = require("./projects-model");
+const checkProjectId = async (req, res, next) => {
+  try {
+    const project = await pDb.get(req.params.id);
+    if (project) {
+      req.project = project;
+      next();
+    } else {
+      next({
+        status: 404,
+        message: `project with id ${req.params.id} not found`,
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+const checkProjectPayload = async (req, res, next) => {
+  if (!req.body.name || !req.body.description || !req.body.completed) {
+    next({
+      status: 400,
+      message: "Missing name,description and completed required fields",
+    });
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  checkProjectId,
+  checkProjectPayload,
+};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,11 +1,30 @@
 // Write your "projects" router here!
 const express = require("express");
+const { checkProjectId, checkProjectPayload } = require("./projects-middleware");
 const Projects = require("./projects-model");
 const router = express.Router();
 
+//`[GET] /api/projects`
 router.get("/", (req, res, next)=>{
     console.log("I am in projects router");
-    res.status(200).json({message:"I am in projects router"})
-})
+    Projects.get()
+    .then(data=>{
+      res.json(data);
+    })
+    .catch(next)
+    })
+//`[GET] /api/projects/:id`
+router.get('/:id', checkProjectId, (req, res, next) => {
+        res.json(req.hub);
+      });
+
+// `[POST] /api/projects`
+router.post('/', checkProjectPayload,(req, res, next) => {
+    Projects.insert(req.body)
+      .then(project => {
+        res.status(201).json(project);
+      })
+      .catch(next);
+  });
 
 module.exports = router;
