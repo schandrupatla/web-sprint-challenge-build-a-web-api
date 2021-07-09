c1e9db20329f185280ecfed183f6e65433705ac5
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-07-09 11:16:53 -0700

    added actions post

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -20,4 +20,13 @@ router.get('/:id', checkActionId,  (req, res, next)=>{
      res.json(req.action);
 })
 
+//`[POST] /api/actions`
+router.post('/', checkActionPayload, (req, res, next) => {
+    Actions.insert(req.body)
+      .then(action => {
+        res.status(201).json(action);
+      })
+      .catch(next);
+  });
+
 module.exports = router;
